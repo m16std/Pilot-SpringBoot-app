cb85bea2cd88e5485af941913d0ebbf3f2b4b3eb
m16std <uwm16@yandex.ru>
2023-04-30 13:36:35 +0300

    FixedTests

--- src/test/java/com/rep/yo/UserControllerTest.java
+++ src/test/java/com/rep/yo/UserControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import com.rep.yo.controllers.FilmController;
+import com.rep.yo.controllers.UserController;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -13,27 +13,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class FilmControllerTest {
+public class UserControllerTest {
     @Autowired
-    private FilmController controller;
-
+    private UserController controller;
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     public void AddTest() throws Exception {
-        this.mockMvc.perform(get("/films/add?name=owo&country=owo&year=0")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/user/add?name=owo&age=18")).andDo(print()).andExpect(status().isOk());
     }
     @Test
     public void GetAllTest() throws Exception {
-        this.mockMvc.perform(get("/films/get")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/user/get")).andDo(print()).andExpect(status().isOk());
     }
     @Test
     public void GetCurrentTest() throws Exception {
-        this.mockMvc.perform(get("/films/get/0")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/user/get/0")).andDo(print()).andExpect(status().isOk());
     }
     @Test
     public void DeleteTest() throws Exception {
-        this.mockMvc.perform(get("/films/delete/0")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/user/delete/0")).andDo(print()).andExpect(status().isOk());
     }
-
 }
